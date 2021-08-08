54de750a3c788fcad799e0dde1e0c07cc5745368
Veto Ramirez <vetorobert3@gmail.com>
2021-08-07 23:35:38 -0700

    Adding video methods

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+
 videos = {}
+
+def abort_if_video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Could not find video...")
 
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        abort()
         return videos[video_id]
 
     def put(self, video_id):
-        return
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
 
 api.add_resource(Video, "/video/<int:video_id>")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/bill")
+#response = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100000})
+#print(response.json())
+#input()
+response = requests.get(BASE + "video/6")
 print(response.json())
