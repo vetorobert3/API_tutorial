61c30050e17ecd807407c5d9a6f6908f8c8e68a2
Veto Ramirez <vetorobert3@gmail.com>
2021-08-09 19:04:45 -0700

    adding video requests

--- test.py
+++ test.py
@@ -2,8 +2,18 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-#response = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100000})
-#print(response.json())
-#input()
-response = requests.get(BASE + "video/6")
+data = [{"likes": 1000, "name": "Idiots in Cars", "views": 100000},
+        {"likes": 782, "name": "Cat Dance", "views": 12355584},
+        {"likes": 187121, "name": "How to make a video", "views": 100000},
+        {"likes": 10, "name": "Tim eats pizza", "views": 10},]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
